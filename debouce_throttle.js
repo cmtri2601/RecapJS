4c4109f399732e2cf5e9b13c8176b39474e1594a
minh-tri <cmtri2601@gmail.com>
2024-10-11 19:40:14 +0700

    done debounce throttle

--- debouce_throttle.js
+++ debouce_throttle.js
@@ -1,8 +1,10 @@
-const nameInput = document.querySelector('#textName');
-const resultArea = document.querySelector('#searchResult');
+const input = document.querySelector('#textName');
+const defaultArea = document.querySelector('#default');
+const debounceArea = document.querySelector('#debounce');
+const throttleArea = document.querySelector('#throttle');
 
-const search = async (name = '') => {
-    const url = `https://api.crossref.org/works?query.title=${name}&rows=10`
+const search = async (name = '', resultArea) => {
+    const url = `https://api.crossref.org/works?query.title=${name}&rows=3`
     try {
         const res = await fetch(url);
         if (!res.ok) {
@@ -12,13 +14,13 @@ const search = async (name = '') => {
         const works = data.message.items.map(item => ({
             title: item.title?.[0],
         }));
-        return works;
+        showResult(works, resultArea);
     } catch (error) {
         console.log('Error: ' + error);
     }
 }
 
-const showResult = (data) => {
+const showResult = (data, resultArea) => {
     let content = "There's no result for this title";
     if (data?.length) {
         content = data.reduce((text, item, index) => {
@@ -30,44 +32,93 @@ const showResult = (data) => {
 }
 
 // search each time keyup
-const normalSearch = async (e) => {
+const handleNormalSearch = (e) => {
     const name = e.target.value;
-    const works = await search(name);
-    showResult(works);
-    
-}
-
-/**
- * Throttle:
- * A throttle function ensures that a function is called at most once 
- * in a specified time period, regardless of how many times the event 
- * is triggered. This can help prevent performance issues by reducing 
- * the number of times a function is executed.
- */
-
-const throttleSearch = async (e) => {
-    const name = e.target.value;
-    const works = await search(name);
-    showResult(works);
-    
+    search(name, defaultArea);
 }
 
 /**
  * Debounce:
- * Debouncing is a programming practice used to ensure that a function 
- * is not called too frequently. It limits the rate at which a function 
- * can fire. This is particularly useful for performance optimization in 
- * scenarios where a function might be called repeatedly in quick succession, 
- * such as during user input events like keystrokes, window resizing, or scrolling.
+ * The debounce technique is a programming pattern used to limit the rate 
+ * at which a function is executed. It ensures that a function is only called 
+ * after a certain amount of time has passed **since the LAST TIME it was INVOKED**. 
+ * This is particularly useful for scenarios where you want to reduce the 
+ * number of times a function is called, such as handling user input events 
+ * like keystrokes or window resizing.
+ * 
+ * Use case: autocomplete filed, search field
  */
-const debounceSearch = async (e) => {
+const debounce = (cb, delay = 1000) => {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            cb(...args);
+        }, delay);
+    }
+}
+
+// create closure for timeout
+const debounceSearch = debounce(search);
+
+const handleDebounceSearch = (e) => {
     const name = e.target.value;
-    const works = await search(name);
-    showResult(works);
+    // can't not put debounceSearch here because it will create new instance of debounceSearch
+    // => can't keep the timeout value
+    // const debounceSearch = debounce(search); 
+    debounceSearch(name, debounceArea);
+}
+
+/**
+ * Throttle:
+ * The throttle technique is a way to control the rate at which a function is executed. 
+ * It ensures that a function is not called more frequently than a **specified INTERVAL**. 
+ * This is particularly useful in scenarios where you want to limit the number of times 
+ * a function is executed over time, such as handling events that fire rapidly.
+ * 
+ * Use case: scroll event, resize event, drag and drop, mousemove
+ */
+const throttle = (cb, delay = 1000) => {
+    let shouldWait = false;
+    let waitingArgs;
+    const timeoutFunc = () => {
+        if (!waitingArgs) {
+            shouldWait = false;
+        } else {
+            cb(...waitingArgs);
+            waitingArgs = null;
+            setTimeout(timeoutFunc, delay);
+        }
+    }
+
+    return function(...args) {
+        if (shouldWait) {
+            waitingArgs = args;
+            return;
+        };
+        
+        cb(...args);
+        shouldWait = true;
+
+        setTimeout(timeoutFunc, delay)
+    }
+}
+
+// create closure for shouldWait
+const throttleSearch = throttle(search);
+ 
+const handleThrottleSearch = (e) => {
+    const name = e.target.value;
+    throttleSearch(name, throttleArea);
     
 }
 
-nameInput.addEventListener('keyup', normalSearch);
+/**
+ * Add event listener
+ */
+input.addEventListener('keyup', handleNormalSearch);
+input.addEventListener('keyup', handleDebounceSearch);
+input.addEventListener('keyup', handleThrottleSearch);
 
 
 
